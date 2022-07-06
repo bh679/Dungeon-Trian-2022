304b5e0bed70b2d13ad77229edc545633fe3b366
Brennan Hatton MSI <bernnan@equalreality.com>
2022-07-06 15:32:15 +1000

    Moving into a new cart, adds another cart down the line. GIving carts physics floors

--- Assets/Brennan Hatton/Dungeon Train/Systems/Train Carts/Scripts/TrainCartManager.cs
+++ Assets/Brennan Hatton/Dungeon Train/Systems/Train Carts/Scripts/TrainCartManager.cs
@@ -7,28 +7,42 @@ public class TrainCartManager : MonoBehaviour
 	public TrainCartGenerator generator;
 	
 	public int totalTrainCarts = 3;
-	public TrainCart[] carts;
+	public List<TrainCart> carts = new List<TrainCart>();
 	
 	float position = 0;
 	
     // Start is called before the first frame update
     void Start()
 	{
-		carts = new TrainCart[totalTrainCarts];
 		
 	    for(int i =0 ;i < totalTrainCarts; i++)
 	    {
-	    	carts[i] = generator.CreateCart();
-	    	
-	    	carts[i].transform.position += transform.forward*position;
-	    	
-	    	position += carts[i].length;
+	    	AddCartToEnd();
 	    }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    if(carts[1].playerInside)
+	    {
+	    	DestroyObject(carts[0].gameObject);
+	    	carts.RemoveAt(0);
+	    	
+	    	AddCartToEnd();
+	    }
     }
+    
+	void AddCartToEnd()
+	{
+		TrainCart newCart;
+		
+		newCart = generator.CreateCart();
+	    	
+		newCart.transform.position += transform.forward*position;
+	    	
+		position += newCart.length;
+	    	
+		carts.Add(newCart);
+	}
 }
