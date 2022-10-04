ff9b8ad1515e5ee8d20a9e09fa18517d1bb360a7
Brennan Hatton - NGNMSI <brennan@brennanhatton.com>
2022-10-05 10:21:02 +1100

    Book Asset - yet to be implmented

---
+++ Assets/Brennan Hatton/Babel/Models/Book/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+	public Animator book, page;
+	
+	bool isOpen;
+	
+	public void bookOpen(){
+		book.SetTrigger("Open");
+		isOpen = true;
+	}
+	
+	public void bookClose(){
+		page.gameObject.SetActive(false);
+		
+		book.SetTrigger("Close");
+		isOpen = false;
+	}
+	
+	public void nextPage()
+	{
+		if(isOpen){
+			page.gameObject.SetActive(true);
+			page.SetTrigger("nextPage");
+		}
+	}
+	
+	public void previousPage()
+	{
+		if(isOpen){
+			page.gameObject.SetActive(true);
+			page.SetTrigger("previousPage");
+		}
+	}
+}
