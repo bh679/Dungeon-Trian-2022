16da0a25b63a9c277b9ea6e55fd7ec65c71e180e
Brennan Hatton MSI <bernnan@equalreality.com>
2023-01-23 11:05:43 +1100

    Adding scanning data to keep them in the prefabs imported from 2021

---
+++ Assets/Brennan Hatton/Scanning System/ItemDescription.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BrennanHatton.Scanner
+{
+	
+	[System.Serializable]
+	public class Line
+	{
+		public string textLine;
+		public AudioClip audioLine;
+		public float delayafter = 0.5f;
+		public SharedLines sharedLines;
+		
+		public void SetSharedLines()
+		{
+			if(sharedLines != null)
+				textLine = sharedLines.GetSharedLine();
+		}
+
+		//this should be moved to display
+		/*public void Play(Text textDisplay, AudioSource audioSource)
+		{
+			/*if(audioLine != null)
+				Debug.Log("Play " + " " + audioLine.name);
+			else
+			//Debug.Log("Play " + " " + audioLine);-//*-/
+				
+			if(textLine != null)
+				textDisplay.text = textLine;
+			else
+				textDisplay.text = "";
+		
+			if(audioSource != null && audioLine != null)
+			{
+				audioSource.PlayOneShot(audioLine);
+				//Debug.Log("audioSource.PlayOneShot(audioLine);");
+			}
+		}*/
+		
+		public float Length(bool plusDelay = false)
+		{
+			
+			if(audioLine != null)
+				return audioLine.length + (plusDelay? delayafter : 0) ;
+				
+			else return textLine.Length * 0.045f + (plusDelay? delayafter : 0);
+		}
+	}
+
+
+	[System.Serializable]
+	public class Description
+	{
+		public Line[] lines;
+	
+		int nextLineNumber = 0;
+		
+		public void StartFromStart()
+		{
+			nextLineNumber = 0;
+		}
+		
+		public bool NoMoreLines()
+		{
+			return (nextLineNumber == 0);
+			
+		}
+	
+		//this should be moved to display
+		public Line GetNextLine()
+		{
+			
+			int retVal = nextLineNumber;
+			//float delay = lines[nextLineNumber].delayafter + lines[nextLineNumber].Length();
+			
+			//lines[nextLineNumber].Play(textDisplay, audioSource); // right now it only plays one line
+			nextLineNumber++;
+
+			nextLineNumber = nextLineNumber % lines.Length;
+			
+			lines[retVal].SetSharedLines();
+			
+			return lines[retVal];
+			
+		}
+		
+		public float TotalLength()
+		{
+			float total = 0;
+			
+			for(int i = 0; i < lines.Length; i++)
+			{
+				total += lines[i].delayafter + lines[i].Length();
+			}
+			
+			return total;
+		}
+		
+		
+	}
+
+	public class ItemDescription : MonoBehaviour
+	{
+		
+		
+		public List<Description> descriptions = new List<Description>();
+		static int i = 0;
+		
+		void Reset()
+		{
+			Description defaultDescription = new Description();
+			defaultDescription.lines = new Line[1];
+			defaultDescription.lines[0] = new Line();
+			defaultDescription.lines[0].textLine = "A " + this.gameObject.name;
+			descriptions.Add(defaultDescription);
+		}
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+	        
+	    }
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+	        
+	    }
+		public Description Scan()
+		{
+			i++;
+			
+			if(i > descriptions.Count -1)
+			 i = 0;
+			
+			
+			if(descriptions.Count <= 1)
+				return descriptions[0];
+			
+			return descriptions[i];
+			
+		}
+	    
+	    
+		/*/this should be moved to display
+		public float Scan(TextMesh textDisplay, AudioSource audioSource)
+		{
+			i++;
+			
+			if(i > descriptions.Length -1)
+			 i = descriptions.Length -1;
+			
+			
+			if(descriptions.Length > 1)
+				StartCoroutine(PlayAllLines(textDisplay, audioSource, i));
+			else
+				StartCoroutine(PlayAllLines(textDisplay, audioSource, 0));
+			
+			return descriptions[i].TotalLength();
+			
+		}
+		
+		//this should be moved to display
+		IEnumerator PlayAllLines(TextMesh textDisplay, AudioSource audioSource, int _i)
+		{
+			descriptions[_i].StartFromStart();
+			float delay;
+			
+			do
+			{
+				delay = descriptions[_i].PlayNextLine(textDisplay, audioSource);
+				
+				yield return new WaitForSeconds(delay);
+				
+			}while(!descriptions[_i].NoMoreLines());
+			
+			yield return null;
+		}*/
+	}
+
+}
---
+++ Assets/Brennan Hatton/Scanning System/TypeDescription.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BrennanHatton.Props;
+using BrennanHatton.Scanner;
+
+namespace BrennanHatton.Scanner.Props
+{
+	
+	public class TypeDescription : MonoBehaviour
+	{
+		public PropType type;
+		public List<Description> sharedDescriptions = new List<Description>();
+		ItemDescription[] descriptions;
+		
+		void Reset()
+		{
+			type = this.GetComponent<PropType>();
+			
+			Description defaultDescriptions = new Description();
+			defaultDescriptions.lines = new Line[1];
+			defaultDescriptions.lines[0] = new Line();
+			defaultDescriptions.lines[0].textLine = "A " + gameObject.name;
+			if(defaultDescriptions.lines[0].textLine[defaultDescriptions.lines[0].textLine.Length-1] == 's')
+				defaultDescriptions.lines[0].textLine = defaultDescriptions.lines[0].textLine.Remove(defaultDescriptions.lines[0].textLine.Length-1);
+			
+			sharedDescriptions.Add(defaultDescriptions);
+		}
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+		    AddDescriptions();
+	    }
+	    
+		void AddDescriptions()
+		{
+			//creates array of item descriptions components
+			descriptions = new ItemDescription[type.propData.Count];
+			
+			//for all props in type
+			//adds item descirption comonents to all props in type 
+			for(int i = 0; i < type.propData.Count; i++)
+			{
+				//try get existing prop description
+				descriptions[i] = type.propData[i].prop.gameObject.GetComponent<ItemDescription>();
+				
+				//check if prop has description
+				if(descriptions[i] == null)
+					//if not, create new description
+					descriptions[i] = type.propData[i].prop.gameObject.AddComponent<ItemDescription>();
+				
+				//add description lines
+				for(int d = 0; d< sharedDescriptions.Count; d++)
+					descriptions[i].descriptions.Add(sharedDescriptions[d]);
+			}
+			
+			
+			/*/props
+			for(int i = 0; i < type.unconvertedProps.Count; i++)
+			{
+				//try get existing prop description
+				descriptions[i+type.propData.Count] = type.unconvertedProps[i].gameObject.GetComponent<ItemDescription>();
+				
+				//check if prop has description
+				if(descriptions[i+type.propData.Count] == null)
+					//if not, create new description
+					descriptions[i+type.propData.Count] = type.unconvertedProps[i].gameObject.AddComponent<ItemDescription>();
+				
+				//add description lines
+				descriptions[i+type.propData.Count].descriptions.AddRange(sharedDescriptions.ToArray());
+			}*/
+			
+		}
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+	        
+	    }
+	}
+
+}
